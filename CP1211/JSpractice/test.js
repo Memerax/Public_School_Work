76d3813522aa0eb37902e6dffff16b7b04191cb4
Memerax <josephwhite@gmx.com>
2023-02-06 20:26:56 -0330

    Practice Examples

---
+++ CP1211/JSpractice/test.js
@@ -0,0 +1,17 @@
+function calculateArea() {
+  let lengthOfField = parseInt(document.querySelector("#length").value)
+  let widthOfField = parseInt(document.querySelector("#width").value)
+  let areaOfField = lengthOfField * widthOfField
+  document.querySelector('#result1').innerHTML = `${areaOfField}`
+}
+
+function calculateReturn() {
+  let numberLessThenLiter = parseFloat(document.querySelector("#lessthenliter").value)
+  let numberGreaterThenLiter = parseFloat(document.querySelector("#greaterthenliter").value)
+  let total = (numberGreaterThenLiter * 0.25) + (numberLessThenLiter * 0.1)
+  document.querySelector("#result2").innerHTML = `${total}`
+}
+
+function resetArea() {
+  document.querySelector('#result1').innerHTML = `0`
+}
