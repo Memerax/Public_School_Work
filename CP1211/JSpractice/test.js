9b95876d410c93cfc5bc32953e2f3b85e1a2c9fa
Memerax <josephwhite@gmx.com>
2023-02-07 11:01:19 -0330

    JS work

--- CP1211/JSpractice/test.js
+++ CP1211/JSpractice/test.js
@@ -8,7 +8,7 @@ function calculateArea() {
 function calculateReturn() {
   let numberLessThenLiter = parseFloat(document.querySelector("#lessthenliter").value)
   let numberGreaterThenLiter = parseFloat(document.querySelector("#greaterthenliter").value)
-  let total = (numberGreaterThenLiter * 0.25) + (numberLessThenLiter * 0.1)
+  let total =(numberGreaterThenLiter * 0.25) + (numberLessThenLiter * 0.1)
   document.querySelector("#result2").innerHTML = `${total}`
 }
 
